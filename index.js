cc537e4d56f3369becaa299b981037e31cc81f7e
Lawson Taylor <lawson.j.taylor@gmail.com>
2018-11-12 02:15:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const projects = [
+  {
+    name: 'Idle Australia',
+    url: '',
+    imageUrl: '',
+    technology: 'MEAN STACK',
+    description: 'Construction hire online',
+  },
+  {
+    name: 'SWAPI',
+    url: '',
+    imageUrl: '',
+    technology: 'ANGULAR',
+    description: 'Starwars',
+  },
+  {
+    name: 'Simple Snake',
+    url: '',
+    imageUrl: '',
+    technology: 'HTML5 CANVAS',
+    description: 'Simple Snake - on html canvas',
+  },
+  {
+    name: 'CHAT ROOM',
+    url: '',
+    imageUrl: '',
+    technology: 'Sockets Chatroom',
+    description: 'Typescipt sockets chatroom',
+  },
+  {
+    name: 'Rumours',
+    url: '',
+    imageUrl: '',
+    technology: 'Typescript Blockchain',
+    description: '',
+  },
+
+];
+
+const body = document.getElementById('projects');
+
